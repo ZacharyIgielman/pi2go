340b5a5969419adcb9abee1403d7ecc1cfa94f0b
Zachary Igielman <zacharyi123@gmail.com>
2014-05-13 19:30:37 +0000

    planned demoalllib.py code

---
+++ library/demoalllib.py
@@ -0,0 +1,24 @@
+#import pi2go and other libs threading, time, sys
+
+#first thread def
+#every 1 second check pi2go.getDistance and save t global varibale
+#threading start first thread
+
+#second thread def
+#while true
+#if distance global is too low or stop global says stop then stop or else:
+#if state==0
+#slow line follower
+#if state==1
+#fast line follower
+#if state==2
+#sonar bot using global distance
+#if state==3
+#ir navigator
+#start with threading
+
+#try
+#get raw input
+#if "" then increment global state (if fastlf then staright)
+#else keyboard code (w,d,a,s or else stop) and global stop stop
+#excpet kbd interrupt stop cleanup sysexit
