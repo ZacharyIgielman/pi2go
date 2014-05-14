67535145b3c96c9ea4cbe462902496e0012120e8
ZacharyIgielman <zacharyi123@me.com>
2014-05-14 10:21:05 +0100

    continued work on demoall with library

--- library/demoalllib.py
+++ library/demoalllib.py
@@ -1,24 +1,71 @@
-#import pi2go and other libs threading, time, sys
+import pi2go, threading, time, sys
 
-#first thread def
-#every 1 second check pi2go.getDistance and save t global varibale
-#threading start first thread
+pi2go.init()
 
-#second thread def
-#while true
-#if distance global is too low or stop global says stop then stop or else:
-#if state==0
-#slow line follower
-#if state==1
-#fast line follower
-#if state==2
-#sonar bot using global distance
-#if state==3
-#ir navigator
-#start with threading
+globalDistance=0
+globalStop=0
+state=0
 
-#try
-#get raw input
-#if "" then increment global state (if fastlf then staright)
-#else keyboard code (w,d,a,s or else stop) and global stop stop
-#excpet kbd interrupt stop cleanup sysexit
+def updateDistance():
+  global globalDistance
+  while True:
+    globalDistance=pi2go.getDistance()
+    time.sleep(0.5)
+
+threading.timer(0.5,updateDistance())
+
+def mainLoop:
+  global globalDistance
+  global globalStop
+  global state
+  while True:
+    if globalStop==1 or globalDistance<10:
+      pi2go.stop()
+    else:
+      if state==0:
+        #slow lf
+      elif state==1:
+        #fast lf
+      elif state==2:
+        #sonar navigator using glpobalDistance
+      elif state==3:
+        #sonar avoider using globalDistance
+      elif state==4:
+        #ir navigator
+        
+threading.timer(0.5,mainLoop())
+
+try:
+  while True:
+    in=raw_input("")
+    globalstop=1
+    if in=="":
+      globalstop=0
+      if state==4:
+        state=0
+      else:
+        state=state+1
+      if state==0:
+        #led colour
+      elif state==1:
+        #straight
+        #led colour
+      elif state==2:
+        #led colour
+      elif state==3:
+        #led colour
+      else:
+        #led colour
+    elif in=="w":
+      pi2go.staright(60)
+    elif in=="a":
+      pi2go.spinLeft(60)
+    elif in=="d":
+      pi2go.spinRight(60)
+    elif in=="s":
+      pi2go.reverse(60)
+    else:
+      pi2go.stop()
+except KeyboardInterrupt:
+  pi2go.cleanup()
+  sys.exit()
