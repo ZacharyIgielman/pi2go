2456511530b3365808bd711726d64150304bcfde
ZacharyIgielman <zacharyi123@me.com>
2014-05-14 17:46:27 +0100

    created main autonomous loop

--- library/demoalllib.py
+++ library/demoalllib.py
@@ -6,39 +6,79 @@ globalDistance=0
 globalStop=0
 state=0
 
+slowspeed = 20
+fastspeed = 100
+
+lastleft = 0
+lastright = 0
+
 def updateDistance():
   global globalDistance
   while True:
     globalDistance=pi2go.getDistance()
-    time.sleep(0.5)
+    time.sleep(0.2)
 
-threading.timer(0.5,updateDistance())
+threading.timer(0.2,updateDistance())
 
 def mainLoop:
-  global globalDistance
-  global globalStop
-  global state
+  global globalDistance, globalStop, state
   while True:
-    if globalStop==1 or globalDistance<10:
+    if globalStop==1 or globalDistance<5:
       pi2go.stop()
     else:
       if state==0:
-        #slow lf
+        if pi2go.irLeftLine() and pi2go.irRightLine():
+          pi2go.forward(40)
+        elif pi2go.irRightLine()==False:
+          pi2go.spinRight(fast)
+        elif pi2go.irLeftLine()==False:
+          pi2go.spinLeft(fast)
       elif state==1:
-        #fast lf
+        global slowspeed, fastspeed, lastleft, lastright
+        left = pi2go.irLeftLine()
+        right = pi2go.irRightLine()
+        if left==0 and right==0:
+          pi2go.stop()
+        if left == 0 and lastleft == 1:
+          pi2go.turnForward(slowspeed,fastspeed)
+        elif right == 0 and lastright == 1:
+          pi2go.turnForward(fastspeed,slowspeed)
+        lastleft = left
+        lastright = right
+        time.sleep(0.01)
       elif state==2:
-        #sonar navigator using glpobalDistance
+        if globalDistance>15:
+		      pi2go.forward(50)
+	      else:
+		      pi2go.reverse(30)
+		      time.sleep(0.5)
+		      pi2go.turnReverse(30,50)
+		      time.sleep(3)
       elif state==3:
-        #sonar avoider using globalDistance
+        if globalDistance>15:
+		      pi2go.forward(50)
+	      else:
+		      pi2go.spinLeft(50)
       elif state==4:
-        #ir navigator
+	      if pi2go.irAll()==False:
+		      pi2go.forward(fast)
+	      else:
+		      ir=pi2go.irRight()
+		      pi2go.reverse(slow)
+		      time.sleep(0.5)
+		      if ir:
+			      pi2go.turnReverse(50,30)
+		      else:
+			      pi2go.turnReverse(30,50)
+          time.sleep(3)
         
-threading.timer(0.5,mainLoop())
+threading.timer(1,mainLoop())
 
 try:
   while True:
     in=raw_input("")
     globalstop=1
+    pi2go.setAllLEDs(4095, 0, 0)
     if in=="":
       globalstop=0
       if state==4:
@@ -46,18 +86,18 @@ try:
       else:
         state=state+1
       if state==0:
-        #led colour
+        pi2go.setAllLEDs(0, 4095, 0)
       elif state==1:
-        #straight
-        #led colour
+        pi2go.straight(60)
+        pi2go.setAllLEDs(4095, 4095, 0)
       elif state==2:
-        #led colour
+        pi2go.setAllLEDs(0, 4095, 4095)
       elif state==3:
-        #led colour
+        pi2go.setAllLEDs(4095, 0, 4095)
       else:
-        #led colour
+        pi2go.setAllLEDs(0, 0, 4095)
     elif in=="w":
-      pi2go.staright(60)
+      pi2go.straight(60)
     elif in=="a":
       pi2go.spinLeft(60)
     elif in=="d":
