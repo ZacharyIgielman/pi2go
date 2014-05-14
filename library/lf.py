d817c8a196a346af1f3a330f03801d691bde07d7
ZacharyIgielman <zacharyi123@me.com>
2014-05-14 07:17:41 +0100

    corrected for library change

--- library/lf.py
+++ library/lf.py
@@ -12,11 +12,11 @@ try:
                           pi2go.forward(fast)
                           pi2go.setAllLEDs(pwmMax, pwmMax, pwmMax)  # Turn LEDs White for Forwards
                           print('straight')
-                  elif pi2go.irLeftLine()==False:
+                  elif pi2go.irRightLine()==False:
                           pi2go.spinRight(fast)
                           pi2go.setAllLEDs(pwmMax, 0, 0) # Turn LEDs Red for Right
                           print('right')
-                  elif pi2go.irRightLine()==False:
+                  elif pi2go.irLeftLine()==False:
                           pi2go.spinLeft(fast)
                           pi2go.setAllLEDs(0, 0, pwmMax) # Turn LEDs Blue to Left
                           print('left')
