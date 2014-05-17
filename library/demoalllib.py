c6e13be006f18d73c61ffd4ab2688967d7a616bd
ZacharyIgielman <zacharyi123@me.com>
2014-05-17 14:47:41 +0100

    improvements

--- library/demoalllib.py
+++ library/demoalllib.py
@@ -12,35 +12,32 @@ fastspeed = 100
 lastleft = 0
 lastright = 0
 
-finished=0
+finished = False
 
 def updateDistance():
   global globalDistance, finished
-  while True:
-    if finished==1:
-      break
+  while finished == False:
     globalDistance=pi2go.getDistance()
     time.sleep(0.2)
 
-threading.Timer(0.2,updateDistance).start()
+threading.Thread(target = updateDistance).start()
 
 def mainLoop():
   global globalDistance, globalStop, state, finished
-  while True:
-    if finished==1:
-      break
+  global slowspeed, fastspeed, lastleft, lastright
+  while finished == False:
     if globalStop==1 or globalDistance<5:
       pi2go.stop()
     else:
-      if state==0:
+      if state==0:  # Standard Line Follower
         if pi2go.irLeftLine() and pi2go.irRightLine():
           pi2go.forward(40)
         elif pi2go.irRightLine()==False:
-          pi2go.spinRight(fast)
+          pi2go.spinRight(fastspeed)
         elif pi2go.irLeftLine()==False:
-          pi2go.spinLeft(fast)
-      elif state==1:
-        global slowspeed, fastspeed, lastleft, lastright
+          pi2go.spinLeft(fastspeed)
+
+      elif state==1:  # Fast Line Follower
         left = pi2go.irLeftLine()
         right = pi2go.irRightLine()
         if left==0 and right==0:
@@ -52,7 +49,8 @@ def mainLoop():
         lastleft = left
         lastright = right
         time.sleep(0.01)
-      elif state==2:
+
+      elif state==2:  # Obstacle avoider (reverses then spins when near object)
         if globalDistance>15:
           pi2go.forward(50)
         else:
@@ -60,12 +58,14 @@ def mainLoop():
           time.sleep(0.5)
           pi2go.turnReverse(30,50)
           time.sleep(3)
-      elif state==3:
+
+      elif state==3:  # Obstacle avoider (spins when near object)
         if globalDistance>15:
           pi2go.forward(50)
         else:
           pi2go.spinLeft(50)
-      elif state==4:
+
+      elif state==4:  # Avoids objects using IR sensors only
         if pi2go.irAll()==False:
           pi2go.forward(50)
         else:
@@ -78,11 +78,11 @@ def mainLoop():
             pi2go.turnReverse(30,50)
           time.sleep(3)
         
-threading.Timer(1,mainLoop).start()
+threading.Thread(target = mainLoop).start()
 
 try:
   while True:
-    inp=input("")
+    inp = raw_input("}}")
     globalstop=1
     pi2go.setAllLEDs(4095, 0, 0)
     if inp=="":
@@ -94,7 +94,7 @@ try:
       if state==0:
         pi2go.setAllLEDs(0, 4095, 0)
       elif state==1:
-        pi2go.straight(60)
+        pi2go.forward(60)
         pi2go.setAllLEDs(4095, 4095, 0)
       elif state==2:
         pi2go.setAllLEDs(0, 4095, 4095)
@@ -103,7 +103,7 @@ try:
       else:
         pi2go.setAllLEDs(0, 0, 4095)
     elif inp=="w":
-      pi2go.straight(60)
+      pi2go.forward(60)
     elif inp=="a":
       pi2go.spinLeft(60)
     elif inp=="d":
@@ -112,7 +112,10 @@ try:
       pi2go.reverse(60)
     else:
       pi2go.stop()
+
 except KeyboardInterrupt:
-  finished=1
+#except:
+  finished = True
+  time.sleep(1) # wait for threads to finish
   pi2go.cleanup()
   sys.exit()
